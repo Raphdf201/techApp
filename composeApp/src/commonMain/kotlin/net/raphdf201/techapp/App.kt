bfc27c999a1d63ea5cf8c7d3bafec81c88e11bce
Raphael Deschenes <deschenesr545@gmail.com>
2024-11-19 17:14:01 -0500

    Logo and connect with google button

--- composeApp/src/commonMain/kotlin/net/raphdf201/techapp/App.kt
+++ composeApp/src/commonMain/kotlin/net/raphdf201/techapp/App.kt
@@ -1,7 +1,11 @@
 package net.raphdf201.techapp
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
@@ -24,7 +29,8 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
     MaterialTheme {
         var textDisp by remember { mutableStateOf<String>("") }
-        val client = HttpClient()
+        var loginGoogle by remember { mutableStateOf(false) }
+        val client = HttpClient() { followRedirects = false }
         val uriHandler = LocalUriHandler.current
         val finalColor: Color = if (isSystemInDarkTheme()) {
             Color(0, 0, 0)
@@ -35,11 +41,22 @@ fun App() {
             modifier = Modifier.fillMaxSize(),
             color = finalColor
         ) {
+            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                AnimatedVisibility(!loginGoogle) {
+                    Button(onClick = { loginGoogle = true }) {
+                        Text("Se connecter avec Google")
+                    }
+                }
+            }
             LaunchedEffect(key1 = Unit) {
                 textDisp = fetchGoogle(client)
             }
-            Text(text = "Text : ${textDisp}")
-            uriHandler.openUri(textDisp)
+            if (loginGoogle) {
+                try {
+                    uriHandler.openUri(textDisp)
+                } catch (_: Exception) {
+                }
+            }
         }
     }
 }
