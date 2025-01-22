cec2b4f0e22ced4fd2a4ca34188c108de4a3b481
Raphdf201 <raphdf201@hotmail.com>
2025-01-22 11:07:32 -0500

    Cancel reorganisation

--- composeApp/src/commonMain/kotlin/net/raphdf201/techapp/App.kt
+++ composeApp/src/commonMain/kotlin/net/raphdf201/techapp/App.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,6 +14,7 @@ import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -28,7 +30,6 @@ import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 import io.ktor.client.HttpClient
 import kotlinx.coroutines.launch
-import net.raphdf201.techapp.composable.mainApp
 import net.raphdf201.techapp.network.changeAttendance
 import net.raphdf201.techapp.network.fetchEventsText
 import net.raphdf201.techapp.network.fetchGoogle
@@ -65,88 +66,92 @@ fun App() {
             textColor = Color.Black
         }
 
-        mainApp(backgroundColor)
-        Column(modifier(), horizontalAlignment = Alignment.CenterHorizontally) {
-            AnimatedVisibility(!tokenValid) {
-                Column(
-                    modifier(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Button({
-                        corouScope.launch {
-                            openUri(
-                                uriHandler,
-                                fetchGoogle(HttpClient { followRedirects = false })
-                            )
+        Surface(
+            Modifier.fillMaxSize(),
+            color = backgroundColor
+        ) {
+            Column(modifier(), horizontalAlignment = Alignment.CenterHorizontally) {
+                AnimatedVisibility(!tokenValid) {
+                    Column(
+                        modifier(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button({
+                            corouScope.launch {
+                                openUri(
+                                    uriHandler,
+                                    fetchGoogle(HttpClient { followRedirects = false })
+                                )
+                            }
+                        }) {
+                            Text("Accéder au site")
                         }
-                    }) {
-                        Text("Accéder au site")
-                    }
-                    Button({
-                        corouScope.launch {
-                            tokenValid = validateToken(jsonClient, token)
+                        Button({
+                            corouScope.launch {
+                                tokenValid = validateToken(jsonClient, token)
+                            }
+                        }) {
+                            Text("Se connecter")
                         }
-                    }) {
-                        Text("Se connecter")
-                    }
-                    /* Button({ corouScope.launch { token = refreshToken(jsonClient, token) } }) {
+                        /* Button({ corouScope.launch { token = refreshToken(jsonClient, token) } }) {
                         Text("Regénérer le token")
                     } */
-                    OutlinedTextField(
-                        // Modifier,
-                        token,
-                        { token = it },
-                        label = { Text("Token", color = textColor) },
-                        colors = TextFieldDefaults.outlinedTextFieldColors(
-                            textColor = textColor,
-                            unfocusedBorderColor = textColor
+                        OutlinedTextField(
+                            // Modifier,
+                            token,
+                            { token = it },
+                            label = { Text("Token", color = textColor) },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                textColor = textColor,
+                                unfocusedBorderColor = textColor
+                            )
                         )
-                    )
+                    }
                 }
-            }
-            AnimatedVisibility(tokenValid) {
-                Column(
-                    modifier(10)
-                ) {
-                    Spacer(Modifier.height(10.dp))
-                    if (eventsList.isNotEmpty()) {
-                        LazyColumn(modifier()) {
-                            items(eventsList.chunked(eventsList.size)) { columnEvents ->
-                                columnEvents.forEach { event ->
-                                    Row(
-                                        modifier(8, 2, textColor),
-                                        verticalAlignment = Alignment.CenterVertically
-                                    ) {
-                                        event.name?.let { Text(it, Modifier, textColor) }
-                                        event.attendance?.getOrNull(0)?.type?.let { type ->
-                                            val buttonColor: ButtonColors = when (type) {
-                                                present -> ButtonDefaults.buttonColors(Color.Green)
-                                                absent -> ButtonDefaults.buttonColors(Color.Red)
-                                                else -> ButtonDefaults.buttonColors(Color.Yellow)
-                                            }
-                                            Button(
-                                                {
-                                                    corouScope.launch {
-                                                        event.id?.let { eventId ->
-                                                            changeAttendance(
-                                                                jsonClient, token,
-                                                                eventId, invertAttendance(type)
-                                                            )
+                AnimatedVisibility(tokenValid) {
+                    Column(
+                        modifier(10)
+                    ) {
+                        Spacer(Modifier.height(10.dp))
+                        if (eventsList.isNotEmpty()) {
+                            LazyColumn(modifier()) {
+                                items(eventsList.chunked(eventsList.size)) { columnEvents ->
+                                    columnEvents.forEach { event ->
+                                        Row(
+                                            modifier(8, 2, textColor),
+                                            verticalAlignment = Alignment.CenterVertically
+                                        ) {
+                                            event.name?.let { Text(it, Modifier, textColor) }
+                                            event.attendance?.getOrNull(0)?.type?.let { type ->
+                                                val buttonColor: ButtonColors = when (type) {
+                                                    present -> ButtonDefaults.buttonColors(Color.Green)
+                                                    absent -> ButtonDefaults.buttonColors(Color.Red)
+                                                    else -> ButtonDefaults.buttonColors(Color.Yellow)
+                                                }
+                                                Button(
+                                                    {
+                                                        corouScope.launch {
+                                                            event.id?.let { eventId ->
+                                                                changeAttendance(
+                                                                    jsonClient, token,
+                                                                    eventId, invertAttendance(type)
+                                                                )
+                                                            }
                                                         }
-                                                    }
-                                                },
-                                                // Modifier,
-                                                colors = buttonColor
-                                            ) { Text(type, Modifier, textColor) }
+                                                    },
+                                                    // Modifier,
+                                                    colors = buttonColor
+                                                ) { Text(type, Modifier, textColor) }
+                                            }
                                         }
                                     }
                                 }
                             }
+                        } else {
+                            Text("Aucun évènement", color = textColor)
                         }
-                    } else {
-                        Text("Aucun évènement", color = textColor)
-                    }
 
+                    }
                 }
             }
         }
