fae6f059b972a33a5bb35e0d9b3bda9c1fe5501c
Raphdf201 <raphdf201@hotmail.com>
2025-02-19 16:51:29 -0500

    Better padding in events list

--- composeApp/src/commonMain/kotlin/net/raphdf201/techapp/App.kt
+++ composeApp/src/commonMain/kotlin/net/raphdf201/techapp/App.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -120,7 +121,7 @@ fun App() {
                                     columnEvents.forEach { event ->
                                         Row(
                                             modifier(8, 2, textColor),
-                                            Arrangement.SpaceEvenly,
+                                            Arrangement.SpaceBetween,
                                             Alignment.CenterVertically
                                         ) {
                                             event.name?.let {
@@ -146,7 +147,7 @@ fun App() {
                                                             )
                                                         }
                                                     },
-                                                    Modifier.padding(1.dp),
+                                                    Modifier.padding(1.dp).offset((-5).dp),
                                                     colors = buttonColor
                                                 ) { Text(type, Modifier, backgroundColor) }
                                             }
