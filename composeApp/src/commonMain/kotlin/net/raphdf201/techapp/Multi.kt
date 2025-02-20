0be1e40df2edd3a416bc593b1063bb3da07b1857
Raphdf201 <raphdf201@hotmail.com>
2025-02-20 16:49:58 -0500

    Add file save and load implementations

---
+++ composeApp/src/commonMain/kotlin/net/raphdf201/techapp/Multi.kt
@@ -0,0 +1,6 @@
+package net.raphdf201.techapp
+
+expect class Multi {
+    fun save(string: String)
+    fun load(): String
+}
--- composeApp/src/iosMain/kotlin/net/raphdf201/techapp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/net/raphdf201/techapp/MainViewController.kt
@@ -1,5 +1,29 @@
 package net.raphdf201.techapp
 
 import androidx.compose.ui.window.ComposeUIViewController
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSHomeDirectory
+import platform.Foundation.NSString
+import platform.Foundation.NSUTF8StringEncoding
+import platform.Foundation.create
+import platform.Foundation.stringWithContentsOfFile
+import platform.Foundation.writeToFile
 
-fun MainViewController(token: String) = ComposeUIViewController { App() }
+fun MainViewController(token: String) {
+    ComposeUIViewController { App(token) }
+}
+
+@OptIn(ExperimentalForeignApi::class)
+actual class Multi {
+    private val directory = NSHomeDirectory() + "tech"
+
+    @OptIn(BetaInteropApi::class)
+    actual fun save(string: String) {
+        NSString.create(string = string).writeToFile(directory, true, NSUTF8StringEncoding, null)
+    }
+
+    actual fun load(): String {
+        return NSString.stringWithContentsOfFile(directory, NSUTF8StringEncoding, null).toString()
+    }
+}
