a9198e56a1f2f9c981ac8ae27d9140d75b37ae6e
Raphdf201 <raphdf201@hotmail.com>
2025-02-25 08:34:42 -0500

    Rollback

--- composeApp/src/commonMain/kotlin/net/raphdf201/techapp/App.kt
+++ composeApp/src/commonMain/kotlin/net/raphdf201/techapp/App.kt
@@ -2,21 +2,37 @@ package net.raphdf201.techapp
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
+import io.ktor.client.HttpClient
+import kotlinx.coroutines.launch
 
 /**
  * The main composable function for the application
@@ -24,7 +40,12 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun App(token: String = "") {
     MaterialTheme {
-        if (!init) init()
+        var accessToken by remember { mutableStateOf(token) }
+        var refreshToken by remember { mutableStateOf("") }
+        var eventsText by remember { mutableStateOf("") }
+        var eventsList by remember { mutableStateOf(listOf<Event>()) }
+        var tokenValid by remember { mutableStateOf(false) }
+        val client by remember { mutableStateOf(HttpClient()) }
         val coroutineScope = rememberCoroutineScope()
         val dark = isSystemInDarkTheme()
         val uriHandler = LocalUriHandler.current
@@ -45,7 +66,48 @@ fun App(token: String = "") {
             Column(modifier(), horizontalAlignment = Alignment.CenterHorizontally) {
                 Spacer(Modifier.height(50.dp))
                 AnimatedVisibility(!tokenValid) {
-                    login(coroutineScope, uriHandler, textColor)
+                    Column(
+                        modifier(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button({
+                            coroutineScope.launch {
+                                openUri(
+                                    uriHandler,
+                                    fetchGoogle(client)
+                                )
+                            }
+                        }) {
+                            Text("Accéder au site", Modifier, textColor)
+                        }
+                        Button({
+                            coroutineScope.launch {
+                                tokenValid = validateToken(client, accessToken)
+                            }
+                        }) {
+                            Text("Se connecter", Modifier, textColor)
+                        }
+                        OutlinedTextField(
+                            accessToken,
+                            { accessToken = it },
+                            Modifier,
+                            label = { Text("Access token", color = textColor) },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                textColor,
+                                unfocusedBorderColor = textColor
+                            )
+                        )
+                        OutlinedTextField(
+                            refreshToken,
+                            { refreshToken = it },
+                            Modifier,
+                            label = { Text("Refresh token", color = textColor) },
+                            colors = TextFieldDefaults.outlinedTextFieldColors(
+                                textColor,
+                                unfocusedBorderColor = textColor
+                            )
+                        )
+                    }
                 }
                 AnimatedVisibility(tokenValid) {
                     Column(
@@ -53,14 +115,69 @@ fun App(token: String = "") {
                     ) {
                         Spacer(Modifier.height(10.dp))
                         if (eventsList.isNotEmpty()) {
-                            events(coroutineScope, textColor, backgroundColor)
+                            LazyColumn(modifier()) {
+                                items(eventsList.chunked(eventsList.size)) { columnEvents ->
+                                    columnEvents.forEach { event ->
+                                        Row(
+                                            modifier(8, 2, textColor),
+                                            Arrangement.SpaceBetween,
+                                            Alignment.CenterVertically
+                                        ) {
+                                            event.name?.let {
+                                                Text(
+                                                    it,
+                                                    Modifier.padding(8.dp),
+                                                    textColor
+                                                )
+                                            }
+                                            event.attendance?.getOrNull(0)?.type?.let { type ->
+                                                val buttonColor: ButtonColors = when (type) {
+                                                    present -> ButtonDefaults.buttonColors(Color.Green)
+                                                    absent -> ButtonDefaults.buttonColors(Color.Red)
+                                                    else -> ButtonDefaults.buttonColors(Color.Yellow)
+                                                }
+                                                Button(
+                                                    {
+                                                        if (tokenValid) {
+                                                            coroutineScope.launch {
+                                                                changeAttendance(
+                                                                    client, accessToken,
+                                                                    event, invertAttendance(type)
+                                                                )
+                                                            }
+                                                            coroutineScope.launch {
+                                                                eventsText =
+                                                                    fetchEventsText(
+                                                                        client,
+                                                                        accessToken
+                                                                    )
+                                                            }
+                                                            eventsList =
+                                                                jsonDecoder.decodeFromString(
+                                                                    eventsText
+                                                                )
+                                                        }
+                                                    },
+                                                    Modifier.padding(1.dp).offset((-6).dp),
+                                                    colors = buttonColor
+                                                ) { Text(type, Modifier, backgroundColor) }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
                         } else {
                             Text("Aucun évènement", color = textColor)
                         }
                     }
                 }
                 Button({
-                    refresh(coroutineScope)
+                    if (tokenValid) {
+                        coroutineScope.launch {
+                            eventsText = fetchEventsText(client, accessToken)
+                        }
+                        eventsList = jsonDecoder.decodeFromString(eventsText)
+                    }
                 }, Modifier) {
                     Text("Refresh", Modifier, textColor)
                 }
@@ -71,7 +188,11 @@ fun App(token: String = "") {
         } else if (eventsText == unauthorized) {
             tokenValid = false
         } else if (eventsText == "") {
-            refresh(coroutineScope)
+            if (tokenValid) {
+                coroutineScope.launch {
+                    eventsText = fetchEventsText(client, accessToken)
+                }
+            }
         }
     }
 }
